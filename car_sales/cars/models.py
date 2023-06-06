64e1e49194175f8452321d6ca57aaa5915b555e1
amirvalie <hosseinvalie@gmail.com>
2023-06-06 22:23:12 +0330

    Added engine_volume fields to the Car model and update max_length of fields

--- car_sales/cars/models.py
+++ car_sales/cars/models.py
@@ -7,10 +7,13 @@ from car_sales.users.models import BaseModel
 
 class Car(BaseModel):
     user                 = models.ForeignKey(BaseModel , on_delete=models.PROTECT)
-    name                 = models.CharField(max_length=100 , verbose_name='car name')
+    name                 = models.CharField(max_length=255 , verbose_name='car name')
+    #"Pseudoisochromatic Tritanopia Simulation Blue" color with 42 characters
     colorـname           = models.CharField(max_length=50 , verbose_name='color name')
     number_of_cylinder   = models.PositiveIntegerField(verbose_name='number of cylinder')
+    engine_volume        = models.PositiveIntegerField(verbse_name='engine volume')
     number_of_passangers = models.PositiveIntegerField(vebose_name='number of passangers') 
     
+
     def __str__(self):
         return car_name

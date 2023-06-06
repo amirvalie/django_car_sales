006e4c60cb14c1de37d8bc527c8f3b5fb3cb75d6
amirvalie <hosseinvalie@gmail.com>
2023-06-06 22:13:17 +0330

    Created Car model in cars app

--- car_sales/cars/models.py
+++ car_sales/cars/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from car_sales.common.models import BaseModel
+from car_sales.users.models import BaseModel
 # Create your models here.
+
+
+
+class Car(BaseModel):
+    user                 = models.ForeignKey(BaseModel , on_delete=models.PROTECT)
+    name                 = models.CharField(max_length=100 , verbose_name='car name')
+    colorـname           = models.CharField(max_length=50 , verbose_name='color name')
+    number_of_cylinder   = models.PositiveIntegerField(verbose_name='number of cylinder')
+    number_of_passangers = models.PositiveIntegerField(vebose_name='number of passangers') 
+    
+    def __str__(self):
+        return car_name

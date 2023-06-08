d267320d7c754e4124309e1014115332440bf4fc
amirvalie <hosseinvalie@gmail.com>
2023-06-08 23:02:28 +0330

    Added Car model to the django admin

--- car_sales/cars/admin.py
+++ car_sales/cars/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Car
+from django.contrib.admin import ModelAdmin
 # Register your models here.
+
+class CarAdmin(ModelAdmin):
+    pass
+
+admin.site.register(Car,CarAdmin)

b8e9da2976711ef3afb0c39fbb94a6a4b99cc243
amirvalie <hosseinvalie@gmail.com>
2023-08-09 10:37:54 +0330

    Add feature:add CarSearchSimpleApi and ListCarApi to the url

--- car_sales/cars/urls.py
+++ car_sales/cars/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('create-car/', apis.CreateCarApi.as_view(), name='create-car'),
     path('update-car/<int:car_id>/', apis.UpdateCarApi.as_view(), name='update-car'),
     path('search/cars/<str:query>/', apis.CarSearchAPI.as_view(), name='search-car'),
+    path('search/car/simple/<str:query>/', apis.CarSearchSimpleAPI.as_view(), name='search-car-simple'),
+	path('list-car', apis.ListCarApi.as_view(), name='list-car')
 ]
